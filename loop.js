035105192068b832c34c1cea119b88e908ca9c19
Mohammod Ali <mohammodali651@gmail.com>
2022-08-04 00:28:34 +0600

    added a function which skip the price more than 200

--- loop.js
+++ loop.js
@@ -22,3 +22,44 @@ for(var i = 581; i <= 623; i++){
     console.log(i)
     i += 1;
 }
+
+// 5. difference between while loop and for loop?
+// => Use a for loop when you know the loop should execute n times. Use a while loop for reading a file into a variable. Use a while loop when asking for user input. Use a while loop when the increment value is nonstandard.
+
+// 6. Declare an array for all the topics that you have learned last few days display then as output
+ 
+var skills = ['html', 'css', 'bootstrap', 'tailwind', 'javascript']
+for(var i = 0; i < skills.length; i++){
+    var skill = skills[i]
+    console.log(skill)
+} 
+
+// 7. create an array for all the mobile phone. Display all the elements of the array by using a while loop
+
+var mobiles = ['myphone', 'symphony', 'maximus', 'samsung', 'oneplus']
+
+var i = 0;
+while(i < mobiles.length){
+    var mobile = mobiles[i]
+    console.log(mobile)
+    i++
+}
+
+// 8. run a loop frm 30 to 86. this will stop if the values get higher than 44
+
+for(var i = 30; i <= 86; i++){
+    if(i == 44){
+        break
+    }
+    console.log(i)
+}
+
+// 9. display the prices if the prices is lower than 200
+var books = [150, 300, 250, 180, 345];
+for(var i = 0; i < books.length; i++){
+    var book = books[i]
+    if(book > 200){
+        continue
+    }
+    console.log(book)
+}

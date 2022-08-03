b24b3622ea1cc33e2fec36081b60e4713528885d
Mohammod Ali <mohammodali651@gmail.com>
2022-08-03 23:55:59 +0600

    show even or odd numbers function added

---
+++ loop.js
@@ -0,0 +1,24 @@
+// 1. display "ajke amar mon valo nei" gor 39 times
+
+for(var i = 1; i <= 39; i++){
+    console.log('ajke amar mon valo nei')
+}
+
+// 2. display numbers between 58 to 98.
+
+for(var i = 58; i <= 98; i++){
+    console.log(i)
+}
+
+// 3. show all even numbers from 412 to 456.
+
+for(var i = 412; i <= 456; i++){
+    console.log(i)
+    i = i + 1;
+}
+
+// 4. show all odd numbers from 581 to 623.
+for(var i = 581; i <= 623; i++){
+    console.log(i)
+    i += 1;
+}
